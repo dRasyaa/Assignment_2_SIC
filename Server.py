a9aa831921009dda17d8d3ff0a44bd529a3ff2d1
Denivo Rasya Abiyyu <denivoabiyyu@gmail.com>
2025-02-25 08:35:49 +0700

    Benerin server.py

--- Server.py
+++ Server.py
@@ -5,7 +5,7 @@ from pymongo.server_api import ServerApi
 app = Flask(__name__)
 
 # # MongoDB
-uri = "mongodb+srv://denivo:QNSmtASH7aPciM1U@assignment2sic.58tod.mongodb.net/?retryWrites=true&w=majority&appName=Assignment2SIC"
+uri = "mongodb+srv://denivo:bismillahbisa@assignment2sic.58tod.mongodb.net/?retryWrites=true&w=majority&appName=Assignment2SIC"
 
 client = MongoClient(uri, server_api=ServerApi('1'))
 
@@ -14,8 +14,8 @@ try:
     client.admin.command('ping')
     print("Ping berhasil! Terkoneksi dengan MongoDB.")
 except Exception as e:
-    print("Terjadi kesalahan:", e)
-
+    print("Terjadi kesalahan:", e)  
+    
 # Database & Collection
 db = client['AssignmentDataBase']
 collection = db['SensorData']
@@ -24,11 +24,11 @@ collection = db['SensorData']
 # Flask
 @app.route('/sensor_data', methods = ['POST'])
 def collect_data():
-    data = request.json
+    data2 = request.json
 
-    collection.insert_one(data)
-    print('hello')
+    collection.insert_one(data2)
+    print('Success')
     return jsonify({"status": "success", "message": "Data saved"}),201
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=8000)
+    app.run(host='0.0.0.0', debug=True, port=5500)
